a6d5f8e6bd0e06d43852d49b7f299f9b8d469f5e
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-11 20:53:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,9 @@
 # Add your variables here
 
-first_number = 8
-second_number = 2
+first_number = 3.14
+second_number = 8
 sum = first_number + second_number
-difference = first_number = second_number
+difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
 
